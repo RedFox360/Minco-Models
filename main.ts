9534427c4331dec14dd6d995ed59128947664b8b
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2022-07-11 18:17:15 -0700

    Re-exports

---
+++ main.ts
@@ -0,0 +1,5 @@
+import profileModel from './profileSchema';
+import serverModel from './serverSchema';
+import profileInServerModel from './profileInServerSchema';
+
+export { profileModel, profileInServerModel, serverModel };
